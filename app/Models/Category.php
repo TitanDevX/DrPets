cf42fce0fe665f471c8b9fddb15f22ff08ab0fee
TitanDev <setr12219@gmail.com>
2025-03-27 05:44:17 +0300

    Add casts to category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\CategoryType;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Translatable\HasTranslations;
 
@@ -9,7 +10,12 @@ class Category extends Model
 {
 
     use HasTranslations;
+
+    protected $casts = [
+        'type' => CategoryType::class
+    ];
     protected $guarded = ['id','created_at','updated_at'];
 
     protected $translatable = ['name'];
+
 }
