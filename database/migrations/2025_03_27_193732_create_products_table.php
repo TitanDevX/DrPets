8f6e94801b6efbea4d0eb38ed426ba251d51e30f
TitanDev <setr12219@gmail.com>
2025-03-30 08:44:35 +0300

    Add description to products table

--- database/migrations/2025_03_27_193732_create_products_table.php
+++ database/migrations/2025_03_27_193732_create_products_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string("name",30);
+            $table->longText('description')->nullable();
             $table->double("price");
             $table->foreignIdFor(Category::class)->constrained()->cascadeOnDelete()->cascadeOnDelete();
             $table->foreignIdFor(Provider::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
