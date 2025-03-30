d7ea7d0ba542c6bfd001e4cf36c3cae849bdf087
TitanDev <setr12219@gmail.com>
2025-03-30 20:00:42 +0300

    Create invoice content model

---
+++ app/Models/InvoiceContent.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InvoiceContent extends Model
+{
+    /** @use HasFactory<\Database\Factories\InvoiceContentFactory> */
+    use HasFactory;
+     protected $guarded = ['id'];
+
+     public function invoice(){
+        return $this->belongsTo(Invoice::class);
+     }
+     public function invoicable(){
+        return $this->morphTo('invoicable');
+     }
+}
