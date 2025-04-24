ce9d02f79f291cf784f69743105ce25455645094
TitanDev <setr12219@gmail.com>
2025-04-24 03:04:10 +0300

    localizations

---
+++ lang/en/notif.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'booking_accepted_title' => 'Service Booking Accepted!',
+    'booking_accepted_body' => 'Your service booking has been accepted by the service provider, please return to the app to complete the payment.',
+    
+    'booking_rejected_title' => 'Service Booking Rejected',
+    'booking_rejected_body' => 'Your service booking has been rejected by the service provider.',
+
+    'booking_cancelled_title' => 'Service Booking Cancelled',
+    'booking_cancelled_body' => 'Your service booking has been cancelled due to payment completion timeout.',
+
+    'payment_successful_title' => 'Your payment is successful',
+    'payment_successful_body' => '',
+
+    'payment_failed_title' => 'Your payment failed',
+    'payment_failed_body' => '',
+
+    'payment_cancelled_title' => 'Your payment has been cancelled',
+    'payment_cancelled_body' => '',
+];
