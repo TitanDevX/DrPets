c8b17d4bc9de62062c7200f842ca5affa3a3c2ad
TitanDev <setr12219@gmail.com>
2025-04-24 03:05:01 +0300

    Payment service

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -2,13 +2,25 @@
 
 namespace App\Services;
 
+use App\Models\Payment;
+
 
 class PaymentService
 {
+    public function __construct(protected StripeService $stripeService)
+    {
+        
+    }
+    public function userPayments($user){
 
+        return Payment::where('user_id' , '=', $user->id)->get();
 
-    public function store($data){
-        $payment = 
+    }
+    public function store($data)
+    {
+        $payment = Payment::create($data);
+        $session = $this->stripeService->createCheckoutSession($payment);
+        return $payment;
     }
 
 }
